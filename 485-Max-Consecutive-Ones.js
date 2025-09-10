d00448f7d6b390e335c6332b5a95251848509254
Magesh <mageshmarch@gmail.com>
2025-09-10 22:13:00 +0530

    solved max consecutive ones problem

---
+++ 485-Max-Consecutive-Ones.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    let max = 0;
+    let current = 0;
+
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===1){
+            current ++;
+            max = Math.max(max,current);
+        }
+        else{
+            current = 0;
+        }
+    }
+    return max
+};
